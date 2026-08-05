ecdda546ea2b3bb38145d5389c37e6f971f16d69
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:51:55 +0000

    Aug 5, 2026, 4:21 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -1,7 +1,7 @@
 'use-client'
 export default function AuthLayout(){
     return(
-        <div className="">
+        <div className="flex jusi">
             <div className="border-b border-r border-neutral-800 dark:border-neutral-400max-w-[40%] h-[20%] m-auto ">
                 Welcome
             </div>

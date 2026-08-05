9f917461764cc8e5e0810164501c42be8c79fe1f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:19:33 +0000

    Aug 5, 2026, 2:49 PM

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/dist/client/link";
-import {Loading}
+import {LoadingAsset } from ''
 export default function NotFound() {
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans h-full gap-4 dark:bg-black">

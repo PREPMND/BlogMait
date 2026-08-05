28bc919d505af962ca666f7731cda073c4765256
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:13:14 +0000

    Aug 5, 2026, 2:43 PM

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans h-full gap-4 dark:bg-black">
             <h1>404 - Page Not Found</h1>
-            <Button render={<Link href="/" />}>
+            <Button className={} render={<Link href="/" />}>
                 <Link href="/">Go back home</Link>
             </Button>
         </div>

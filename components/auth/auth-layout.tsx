1bffda4f3c13774e00bac7f5e56e42340881a322
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:58:03 +0000

    Aug 5, 2026, 4:28 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -1,27 +1,29 @@
 'use-client'
 
-import { Tabs } from "@base-ui/react"
+import { Tabs ,} from "@base-ui/react"
 import { useState } from "react"
 
-export default function AuthLayout(){
-    const [activeTab,setactiveTab]=useState('login')
+export default function AuthLayout() {
+    const [activeTab, setactiveTab] = useState('login')
 
-    return(
+    return (
         <div className="flex justify-center items-center min-h-[60vh]">
             <div className="border-b border-r border-neutral-800 dark:border-neutral-400 ">
                 Welcome
             </div>
             <Tabs value={activeTab} onValueChange={setactiveTab} className="border-b border-r border-neutral-800 dark:border-neutral-400">
-                <Tabs.List>
-                    <Tabs.Trigger value="login">Login</Tabs.Trigger>
-                    <Tabs.Trigger value="register">Register</Tabs.Trigger>
-                </Tabs.List>
-                <Tabs.Content value="login">
+                <TabsList>
+                    <TabsTrigger value="login">Login</TabsTrigger>
+                    <TabsTrigger value="register">Register</TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="login">
                     <p>Login content goes here.</p>
-                </Tabs.Content>
-                <Tabs.Content value="register">
+                </TabsContent>
+
+                <TabsContent value="register">
                     <p>Register content goes here.</p>
-                </Tabs.Content>
+                </TabsContent>
             </Tabs>
         </div>
     )

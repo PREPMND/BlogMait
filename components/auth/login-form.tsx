96fc0182011710c009a1ee3c3a0c12e5bdc4b790
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 15:15:19 +0000

    Aug 5, 2026, 8:45 PM

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
-import { toast } from "@/components/ui/label";
+import { toast } from "@/components/ui/toast";
 const LoginSchema = {
     email: string().min(5).max(30).contains("@").contains("."),
     password: string().min(8).max(30),

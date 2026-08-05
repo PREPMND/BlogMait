90c9d1975dd62a94ff7563019f0f6d027d8b5a4b
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 14:15:07 +0000

    Aug 5, 2026, 7:45 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout() {
 
     return (
         <div className="flex flex-col justify-center  items-center min-h-full min-w-full">
-            <div className=" md:w-[40%] w-[] h-20 mt-10 md:text-3xl text-2xl font-medium flex items-center justify-center  rounded-md
+            <div className=" md:w-[40%] w-[50%] h-20 mt-10 md:text-3xl text-2xl font-medium flex items-center justify-center  rounded-md
             bg-neutral-800 ">
                 Welcome
             </div>

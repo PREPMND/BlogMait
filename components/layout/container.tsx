0269d85d1b04657ce73ef43d1109af1833d89e93
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 08:25:22 +0000

    Aug 5, 2026, 1:55 PM

---
+++ components/layout/container.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+//cn merges enerything in class
+interface containerProps{
+    children: ReactNode;
+    className?:string;
+}
+export default function Container({children,className}: containerProps)
+{
+    return (
+        <div className={cn("container mx-auto px-4", className)}>
+            <div>
+                {children}
+            </div>
+        </div>
+    )
+}
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,18 +1,2 @@
-import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
-//cn merges enerything in class
-interface containerProps{
-    children: ReactNode;
-    className?:string;
-}
-export default function Container({children,className}: containerProps)
-{
-    return (
-        <div className={cn("container mx-auto px-4", className)}>
-            <div>
-                {children}
-            </div>
-        </div>
-    )
-}
+
 //so cn("",className) ensure all the manually added and classes passed as arguments are applied as they are mergred

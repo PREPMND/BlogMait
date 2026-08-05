4c609b366c888d09f624ac91d16d86ba3272b820
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 15:22:04 +0000

    Aug 5, 2026, 8:52 PM

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -81,7 +81,7 @@ export default function LoginForm() {
             </div>
             <div>
                 {!errorLogin && (
-                    <div className="flex items-center gap-2 w-[20px] rounded-md bg-green-100 px-3 py-2 text-green-700">
+                    <div className="flex items-center gap-2 w-[250px] rounded-md bg-green-100 px-3 py-2 text-green-700">
                         <span>✅</span>
                         <span>Login <span className="hidden md:flex">successful!</span></span>
                     </div>

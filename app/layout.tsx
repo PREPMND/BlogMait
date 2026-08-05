febb894c3abbf0bde63815bae0ed2bc442935de4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:29:36 +0000

    Aug 5, 2026, 3:59 PM

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${playfair.variable}  h-full antialiased`}
+      className={`${popp} ${playfair.variable}  h-full antialiased`}
     >
       <body className="h-full flex flex-col">
         <ThemeProvider attribute={'class'}  defaultTheme="system" enableSystem disableTransitionOnChange containerClassName=" md:w-[70%] w-[95%] border-l-1 border-r-1 min-h-[85vh] border-gray-300 rounded-md shadow-md shadow-gray-300 dark:shadow-gray-700 dark:border-gray-700">

dbee5ddbd8280f6dfb57ed1b0cff24c9735afd68
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 08:33:21 +0000

    Aug 5, 2026, 2:03 PM

--- components/theme/theme-provider.tsx
+++ components/theme/theme-provider.tsx
@@ -2,6 +2,8 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import Header from "../layout/header"
 import { cn } from "@/lib/utils"
+
+
 export function ThemeProvider({
     children,
     containerClassName,

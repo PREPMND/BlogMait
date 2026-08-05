af83d663795d119fa4cc837a6cf7025e372bec9c
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 08:40:45 +0000

    Aug 5, 2026, 2:10 PM

--- components/theme/theme-provider.tsx
+++ components/theme/theme-provider.tsx
@@ -2,6 +2,7 @@
 import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes"
 import Header from "../layout/header"
 import { cn } from "@/lib/utils"
+import { Footer } from "../layout/footer";
 
 interface ExtendedThemeProviderProps extends ThemeProviderProps{
     containerClassName?:string;
@@ -18,7 +19,7 @@ export function ThemeProvider({
             <main className={cn("container mx-auto px-4",containerClassName)}>
                 {children}
             </main>
-            
+            <Footer/>
         </NextThemesProvider>
     )
 }

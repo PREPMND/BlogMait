1305f3f7761ab2b73d7ca997e0502aa6a6cd50d6
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 11:05:38 +0000

    Aug 5, 2026, 4:35 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -7,7 +7,7 @@ export default function AuthLayout() {
     const [activeTab, setactiveTab] = useState('login')
 
     return (
-        <div className="flex justify-center items-center min-h-[60vh]">
+        <div className="flex flex-col justify-center items-center min-h-[60vh]">
             <div className="border-b border-r border-neutral-800 dark:border-neutral-400 ">
                 Welcome
             </div>

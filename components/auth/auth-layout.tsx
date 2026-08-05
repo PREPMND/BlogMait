d03f8924a237ebf500505c966d46f8be47dffa30
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 15:37:00 +0000

    Aug 5, 2026, 9:07 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -13,7 +13,7 @@ export default function AuthLayout() {
             dark:bg-neutral-800  ">
                 Welcome
             </div>
-            <Tabs value={activeTab} onValueChange={setactiveTab} className="w-[70%] *:text-[18px] mt-4">
+            <Tabs value={activeTab} onValueChange={setactiveTab} className="w-[60%] *:text-[18px] mt-4">
                 <TabsList className="w-full *:text-[18px]">
                     <TabsTrigger value="login">Login</TabsTrigger>
                     <TabsTrigger value="register">Register</TabsTrigger>

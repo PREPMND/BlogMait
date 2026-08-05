b0f083f6460088f1b9fd70c14a2a0984c24c94f4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:37:19 +0000

    Aug 5, 2026, 3:07 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,8 +3,8 @@ export default function Header()
 {
     return (
         <div className="w-full sticky top-1 md:w-[90%] mx-auto h-16 flex justify-center items-center">
-            <div>
-            Header
+            <div className="w-full h-full flex justify-between items-center px-4 md:px-0">
+            
             </div>
         </div>
     )

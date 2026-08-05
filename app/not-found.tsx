14ea7eff350f1ad09b873558538591ce1e9c711f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:07:46 +0000

    Aug 5, 2026, 2:37 PM

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function NotFound() {
     return (
         <div>
             <h1>404 - Page Not Found</h1>
-            <butt
+            <Button/>
         </div>
     )
 }

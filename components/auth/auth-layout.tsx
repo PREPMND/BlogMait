3cdcaf2283221a18de001a85384424ac15190a6f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:56:36 +0000

    Aug 5, 2026, 4:26 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -11,7 +11,7 @@ export default function AuthLayout(){
             <div className="border-b border-r border-neutral-800 dark:border-neutral-400 ">
                 Welcome
             </div>
-            <Tabs ></Tabs>
+            <Tabs value={activeTab}></Tabs>
         </div>
     )
 }

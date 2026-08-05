0352feb2972bec581f994a6f4a446bf88e5b18c4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 08:57:38 +0000

    Aug 5, 2026, 2:27 PM

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer(){
     return (
-        <div className="mt-10">
+        <div className="mt-10 w-full md:w-[90%] mx-auto h-16 flex justify-center items-center">
             Footer
         </div>
     )

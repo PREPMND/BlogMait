c187039d5e1d0b23c2e6c0f018ff42f0cd6b25e7
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 14:16:08 +0000

    Aug 5, 2026, 7:46 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout() {
     return (
         <div className="flex flex-col justify-center  items-center min-h-full min-w-full">
             <div className=" md:w-[40%] w-[50%] md:h-20 h-16 mt-10 md:text-3xl text-2xl font-medium flex items-center justify-center  rounded-md
-            bg-neutral-800 ">
+            dark:bg-neutral-800 bg-white ">
                 Welcome
             </div>
             <Tabs value={activeTab} onValueChange={setactiveTab} className="w-[70%] mt-4">

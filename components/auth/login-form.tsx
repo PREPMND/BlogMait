c325fc0cad4156c523743a646d35e737581d91a4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 15:25:11 +0000

    Aug 5, 2026, 8:55 PM

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -57,7 +57,7 @@ export default function LoginForm() {
     };
 
     return (
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 *:text-[18px] flex flex-col justify-center  w-[60%]">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 *:text-[18px] flex flex-col justify-center  max-w-[60%]">
             <div className="space-y-2">
                 <Label htmlFor="email">Email</Label>
                 <Input

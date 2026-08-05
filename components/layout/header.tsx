cedbfbc778689445f31d80d6d98823f04a8e9786
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:08:07 +0000

    Aug 5, 2026, 3:38 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,5 +1,6 @@
 import { Search } from "lucide-react"
 import Link from "next/link"
+import { Button } from "@/components/ui/button"
 export default function Header() {
     const navItems = [
         {

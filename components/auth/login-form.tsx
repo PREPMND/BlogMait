5ce83fef30610f65890d81d1b6a40bc79566c12b
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 15:27:31 +0000

    Aug 5, 2026, 8:57 PM

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -71,7 +71,7 @@ export default function LoginForm() {
             <div className="space-y-2 max-w-[70%] ">
                 <Label htmlFor="password">Password</Label>
                 <Input
-                    className="min-h-["
+                    className="min-h-[38px]"
                     id="password"
                     type="password"
                     placeholder="Enter your password"

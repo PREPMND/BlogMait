879d3655641a70701e591d8ba67464cadda8614d
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:00:35 +0000

    Aug 5, 2026, 3:30 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,3 +1,4 @@
+import { Search } from "lucide-react"
 import Link from "next/link"
 export default function Header() {
     const navItems = [
@@ -33,7 +34,7 @@ export default function Header() {
             </div>
             <div className="flex justify-center items-center gap-2 md:gap-4">
                 <div className="hidden md:block">
-                    
+                    <Search
                 </div>
             </div>
         </div>

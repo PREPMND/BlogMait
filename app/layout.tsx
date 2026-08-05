f7beea835478a5b55f1d9ddb0241e23df9fd2745
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 08:39:12 +0000

    Aug 5, 2026, 2:09 PM

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ThemeProvider attribute={'class'} defaultTheme="system" enableSystem >
+        <ThemeProvider attribute={'class'} defaultTheme="system" enableSystem disableTransition>
           {children}
         </ThemeProvider>
       </body>

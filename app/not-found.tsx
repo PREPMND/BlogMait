89e3a0b28c613425bd453fa55de30f79a5aa715b
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:08:59 +0000

    Aug 5, 2026, 2:38 PM

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/dist/client/link";
 
 export default function NotFound() {
     return (
-        <div>
+        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans  dark:bg-black">
             <h1>404 - Page Not Found</h1>
             <Button asChild>
                 <Link href="/">Go back home</Link>

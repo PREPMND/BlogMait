555fbdac6eb0279352dc226f070c53d4e77fe1c5
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:38:41 +0000

    Aug 5, 2026, 3:08 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,9 +4,9 @@ export default function Header()
 {
     return (
         <div className="w-full sticky top-1 md:w-[90%] mx-auto h-16 flex justify-center items-center">
-            <div className="flex items-center gap-6 >
+            <div className="flex items-center gap-6" >
             <Link href="/" > Go to HomePage </Link>
-            
+            <nav
             </div>
         </div>
     )

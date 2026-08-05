316bc6e33488679bfa9ce31aa3f9ad7631b49c53
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 11:01:32 +0000

    Aug 5, 2026, 4:31 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -1,6 +1,6 @@
 'use-client'
 
-import { Tabs , TabsList ,TabsTrigger} from "@/components/ui/tabs"
+import { Tabs , TabsList ,TabsTrigger,TabsContent} from "@/components/ui/tabs"
 import { useState } from "react"
 
 export default function AuthLayout() {

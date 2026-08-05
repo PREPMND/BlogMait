5a0edde3198b72e10fc36cd8b802c5fda554e47f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:50:21 +0000

    Aug 5, 2026, 3:20 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
 
 
     return (
-        <div className="w-full sticky top-1 md:w-[90%] mx-auto h-16 flex justify-center items-center">
+        <div className="w-full sticky mb-4 top-1 md:w-[90%] mx-auto h-16 flex justify-center items-center">
             <div className="flex items-center w-full" >
                 <Link className="text-3xl font-bold ml-2 mr-3" href="/">Logo</Link>
                 <nav className="flex justify-evenly items-center gap-4 w-full text-[14px] md:text-[18px]  text-gray-700">

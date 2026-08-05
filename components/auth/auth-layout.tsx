4b020d8c8c89b178cceec7c651729ea94cff55c1
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 11:08:52 +0000

    Aug 5, 2026, 4:38 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout() {
 
     return (
         <div className="flex flex-col justify-center items-center min-w-full bg-class ">
-            <div className="border-b border-r border-neutral-800 dark:border-neutral-400w-[40%] rounded-md
+            <div className="border-b border-r border-neutral-800 dark:border-neutral-400w-[100%] rounded-md
             bg-neutral-800 ">
                 Welcome
             </div>

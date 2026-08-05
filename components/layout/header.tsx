38d14f49dbed0132466fd6c663632b6ba10e5ec1
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:14:02 +0000

    Aug 5, 2026, 3:44 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
     return (
         <div className="w-full sticky top-2  md:w-[95%] mx-auto h-18 flex justify-center items-center">
             <div className="flex items-center h-full gap-6 md:w-[60%]" >
-                <Link className="text-3xl hover:underline h font-medium  ml-2 md:mr-3" href="/">Logo</Link>
+                <Link className="text-3xl hover:underline h font-medium  ml-2 md:mr-3" href="/">Home</Link>
                 <nav className="flex justify-evenly items-center gap-4 w-full text-gray-700">
                     {
                         navItems.map((item) => (

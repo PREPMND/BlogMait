44468f79231afe83f42af1504ba66f403dad5612
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:57:34 +0000

    Aug 5, 2026, 3:27 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -17,12 +17,12 @@ export default function Header() {
         <div className="w-full sticky top-2  md:w-[90%] mx-auto h-18 flex justify-center items-center">
             <div className="flex items-center h-full w-full" >
                 <Link className="text-3xl hover:underline h font-[500]  ml-2 md:mr-3" href="/">Logo</Link>
-                <nav className="flex justify-evenly items-center gap-4 w-full text-[16px] md:text-[19px]  text-gray-700">
+                <nav className="flex justify-evenly items-center gap-4 w-full text-gray-700">
                     {
                         navItems.map((item) => (
                             <Link className={`${item.label=='Contact'?'hidden md:flex':'flex'}
                             border-b-2 border-r-2 border-gray-200 
-                            
+                             text-[16px] md:text-[20px] 
                             hover:border-gray-400 transition-all duration-300 py-1 px-2 text-[15px] rounded-md
                             `} key={item.label} href={item.href}>
                                 {item.label}

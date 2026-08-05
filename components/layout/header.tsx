7a4f002a50b2596157c3aa004df8ab1bbe3e710d
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:11:16 +0000

    Aug 5, 2026, 3:41 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
                 </nav>
             </div>
             <div className="flex justify-center items-center gap-2 md:gap-4">
-                <div className="hidden  md:flex">
+                <div className="hidden  md:flex md:items-center">
                     <input className="min-w-[20%] border border-gray-300 rounded-md py-1 px-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Search...">
                     </input>
                     <Search/>

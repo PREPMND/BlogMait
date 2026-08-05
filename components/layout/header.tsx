7d61b893b14484c40c59b203ee93e9ffff0f0019
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:15:42 +0000

    Aug 5, 2026, 3:45 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
 
     return (
         <div className="w-full sticky top-2  md:w-[95%] mx-auto h-18 flex justify-between items-center">
-            <div className="flex items-center h-full gap-6 md:w-[67%]" >
+            <div className="flex items-center h-full md:w-[67%]" >
                 <nav className="flex justify-evenly items-center gap-4 w-full text-gray-700">
                     {
                         navItems.map((item) => (

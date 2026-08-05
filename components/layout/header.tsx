916eef0e88663a9806a60c6c139cdf80e2e9722a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 08:56:29 +0000

    Aug 5, 2026, 2:26 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,7 +2,7 @@
 export default function Header()
 {
     return (
-        <div>
+        <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-300">
             <div>
             Header
             </div>

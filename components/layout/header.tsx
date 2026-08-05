798e3f3e040e9fb8ddd91a1189d53943da0611d4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:53:06 +0000

    Aug 5, 2026, 3:23 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
                         navItems.map((item) => (
                             <Link className={`${item.label=='Contact'?'hidden md:flex':'flex'}
                             border-b-2 border-r-2 border-transparent 
-                            
+                            border-gray-300
                             hover:border-gray-400 transition-all duration-300 py-1 px-2 text-[15px] rounded-md
                             `} key={item.label} href={item.href}>
                                 {item.label}

dcabc79e5ec5713733ddada768160c2e5eeeea5a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:56:14 +0000

    Aug 5, 2026, 4:26 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -3,8 +3,8 @@
 import { Tabs } from "@base-ui/react"
 
 export default function AuthLayout(){
+    const [activeTab,setactiveTab]=useS
 
-    
     return(
         <div className="flex justify-center items-center min-h-[60vh]">
             <div className="border-b border-r border-neutral-800 dark:border-neutral-400 ">

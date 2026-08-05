e9eae4de9750d6d809635fa999ef5858aa60da24
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:05:21 +0000

    Aug 5, 2026, 2:35 PM

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import PostDetail from "./post/[slug]/page";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <PostDetail/>
     </div>
   );

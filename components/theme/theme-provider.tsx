28d42bcd7a7c8461a5f33127fff86b80f8603513
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:34:04 +0000

    Aug 5, 2026, 3:04 PM

--- components/theme/theme-provider.tsx
+++ components/theme/theme-provider.tsx
@@ -16,7 +16,7 @@ export function ThemeProvider({
     return (
         <NextThemesProvider {...props}>
             <Header/>
-            <main className={cn("min-h-full mx- flex flex-col",containerClassName)}>
+            <main className={cn("min-h-full mx-auto flex flex-col",containerClassName)}>
                 {children}
             </main>
             <Footer/>

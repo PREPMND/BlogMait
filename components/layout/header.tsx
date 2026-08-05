2145b8536ffcde603c1aa5f3255cba913c2b5ecd
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:09:16 +0000

    Aug 5, 2026, 3:39 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -34,7 +34,7 @@ export default function Header() {
                 <div className="hidden md:block">
                     <Search/>
                 </div>
-                <Button className="hidden text-[] md:block" variant="destructive">Login</Button>
+                <Button className="hidden text-[18px] md:block" variant="destructive">Login</Button>
             </div>
         </div>
     )

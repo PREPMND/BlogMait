015d4d672fec352c1d976ba29cfbff9a85074074
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 14:34:11 +0000

    Aug 5, 2026, 8:04 PM

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -2,7 +2,8 @@ import { string } from "@preplabs/validator";
 import {z} from "zod";
 
 const LoginSchema=({
-    email:string
+    email:string;
+    password:string;
 })
 export default function LoginForm(){
     return (

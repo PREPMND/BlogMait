2732ae683b9d147f02dc62aa38132ec6b2b1a721
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:31:36 +0000

    Aug 5, 2026, 3:01 PM

--- components/theme/theme-provider.tsx
+++ components/theme/theme-provider.tsx
@@ -14,7 +14,7 @@ export function ThemeProvider({
     ...props
 }:ExtendedThemeProviderProps){
     return (
-        <NextThemesProvider  {...props}>
+        <NextThemesProvider className={className} {...props}>
             <Header/>
             <main className={cn(" mx-auto",containerClassName)}>
                 {children}

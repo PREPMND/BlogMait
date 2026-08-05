32c5931e56f42465ad07fbd76380877e27347fd5
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 08:24:57 +0000

    Aug 5, 2026, 1:54 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -10,7 +10,7 @@ export default function Container({children,className}: containerProps)
     return (
         <div className={cn("container mx-auto px-4", className)}>
             <div>
-
+                {children}
             </div>
         </div>
     )

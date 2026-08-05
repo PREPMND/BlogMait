fb3330d0aca5f93c8365dee5c26e28a1006f3a1a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:57:45 +0000

    Aug 5, 2026, 3:27 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
                             <Link className={`${item.label=='Contact'?'hidden md:flex':'flex'}
                             border-b-2 border-r-2 border-gray-200 
                              text-[16px] md:text-[20px] 
-                            hover:border-gray-400 transition-all duration-300 py-1 px-2 text-[15px] rounded-md
+                            hover:border-gray-400 transition-all duration-300 py-1 md:py-1 px-2 text-[15px] rounded-md
                             `} key={item.label} href={item.href}>
                                 {item.label}
                             </Link>

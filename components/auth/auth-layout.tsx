0d5974fa515f18ba13934196e7a598dceb5de297
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:51:50 +0000

    Aug 5, 2026, 4:21 PM

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -1,7 +1,7 @@
 'use-client'
 export default function AuthLayout(){
     return(
-        <div>
+        <div className="">
             <div className="border-b border-r border-neutral-800 dark:border-neutral-400max-w-[40%] h-[20%] m-auto ">
                 Welcome
             </div>

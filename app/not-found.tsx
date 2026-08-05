e420c56f6a0556183d38c0fa449b3f531a4e463b
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:07:53 +0000

    Aug 5, 2026, 2:37 PM

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <div>
             <h1>404 - Page Not Found</h1>
-            <Button/>
+            <Button asChild/>
         </div>
     )
 }

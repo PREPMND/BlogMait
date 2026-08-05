282f11c1cc00f2b2dd50aa1087d3154fe97eb2b1
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:38:06 +0000

    Aug 5, 2026, 3:08 PM

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,11 +1,11 @@
-import Link from "next/dist/client/link"
+import Link from "next/link"
 
 export default function Header()
 {
     return (
         <div className="w-full sticky top-1 md:w-[90%] mx-auto h-16 flex justify-center items-center">
             <div className="flex items-center gap-6 >
-            <Link href="/"></Link>
+            <Link href="/"> </Link>
             
             </div>
         </div>

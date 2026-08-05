240fdd0119c0764c82307a9014b0b9f9985131bf
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 14:42:31 +0000

    Aug 5, 2026, 8:12 PM

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -3,7 +3,7 @@ import {z} from "zod";
 
 const LoginSchema=({
     email:string().min(5).max(30).contains("@").contains("."),
-    password:string().min(8).max(30).contains(""),
+    password:string().min(8).max(30),
 })
 export default function LoginForm(){
     return (

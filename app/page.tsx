f36c29548a6a2d75157c3c4d489c3428b2751815
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 09:04:30 +0000

    Aug 5, 2026, 2:34 PM

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import PostDetail from "./post/[slug]/page";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Post
+      <PostDetail/>
     </div>
   );
 }

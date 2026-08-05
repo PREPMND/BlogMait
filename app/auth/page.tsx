65b77e3ddc76edf402588d7c6b3bc4c14c140b58
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-05 10:47:19 +0000

    Aug 5, 2026, 4:17 PM

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,8 +1,10 @@
+import AuthLayout from "@/components/auth/auth-layout";
+
 export default function Authpage(){
     return(
         <>
         <div>
-            Authpage
+            <AuthLayout/>
         </div>
         </>
     )

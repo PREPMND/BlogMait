02d1b4a172ba7f924b1c58fd3ff06dd7184dc7e9
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 14:29:29 +0000

    Aug 6, 2026, 7:59 PM

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,8 +1,11 @@
+import BlogCard from "@/components/ui/card-tactical";
+
 export default function PostDetail(){
     return(
         <>
         <div>
             post detail
+            <BlogCard title
         </div>
         </>
     )

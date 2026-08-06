daf210cec8caa96271eebb8496a58747f74a3aec
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 07:08:53 +0000

    Aug 6, 2026, 12:38 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -37,4 +37,7 @@ export const posts = pgTable('posts',{
     description:varchar('description', { length: 255 }).notNull(),
     slug:varchar('slug', { length: 255 }).notNull().unique(),
     authorId:varchar('authour_id', { length: 255 }).references(() => users.id).notNull(),
+    
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })

a976cc167ef04951d55e3c65ea9a9b46d7ba6b27
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 14:48:47 +0000

    Aug 6, 2026, 8:18 PM

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -2,10 +2,11 @@ import Perfect3DTiltCard from "@/components/ui/card-tactical";
 import BlogCard from "@/components/ui/card-tactical1";
 import { title } from "process";
 import { describe } from "zod/v4/core";
+import { id } from "zod/v4/locales";
 
 export default function PostDetail(){
     const Posts=[{
-        title:'hey',description:'hey',slug:'hey',id
+        title:'hey',description:'hey',slug:'hey',id=
     }]
     return(
         <>

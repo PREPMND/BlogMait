418e8e9264f77f9e335e0216051191cd576ee13d
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 14:52:46 +0000

    Aug 6, 2026, 8:22 PM

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -24,7 +24,7 @@ export default function PostDetail(){
         ]
     return(
         <>
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-2 gap">
             {Posts.map((post)=>(
                 <div key={post.id}>
                     <Perfect3DTiltCard title={post.title} description={post.description} slug={post.slug} id={post.id}/>

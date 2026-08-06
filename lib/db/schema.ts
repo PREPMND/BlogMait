e5d0aa3cf371d181db75c63b5e2d921f5e25b08a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 06:50:19 +0000

    Aug 6, 2026, 12:20 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,9 +1,9 @@
-import {pgTable,varchar} from 'drizzle-orm/pg-core'
-import { boolean } from 'zod'
+import {pgTable,varchar,boolean} from 'drizzle-orm/pg-core'
+import {  } from 'zod'
 
 export const users = pgTable('users',{
     id:varchar('id',{length:255}).primaryKey(),
     name:varchar('name',{length:255}).notNull(),
     email:varchar('email',{length:255}).notNull().unique(),
-    emailVerified:boolean("email_verfiied").default()
+    emailVerified:boolean("email_verfiied").default(false)
 })

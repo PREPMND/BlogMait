25ecb7a2b13be922ae3eac9789bbee38f21af03e
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 11:39:48 +0000

    Aug 6, 2026, 5:09 PM

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import {Config} from "drizzle-kit";
 
 export default ({
     dialect:"postgresql",
-    schema:'./src/',
+    schema:'./lib/schema',
     out:"./drizzle",
     dbCredentials:{
         url:process.env.DATABASE_URL || '',

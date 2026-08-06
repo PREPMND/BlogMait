59fb591ac58e1c1b8016b53c8c5dc7e1f903ee78
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 06:48:25 +0000

    Aug 6, 2026, 12:18 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -4,5 +4,5 @@ import {pgTable} from 'drizzle-orm/pg-core'
 export const users = pgTable('users',{
     id:varchar('id',{length:255}).primaryKey(),
     name:varchar('name',{length:255}).notNull(),
-    
+    email:varchar('email',{})
 })

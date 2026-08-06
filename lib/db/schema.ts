e34dcb7f3d50b8a0a4fbc2be480e16053227a8a4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 07:12:00 +0000

    Aug 6, 2026, 12:42 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -31,7 +31,7 @@ export const accounts = pgTable('accounts', {
 })
 
 export const posts = pgTable('posts',{
-    id: serial('id', { length: 255 }).primaryKey(),
+    id: serial('id').primaryKey(),
     title: varchar('title', { length: 255 }).notNull(),
     description:varchar('description', { length: 255 }).notNull(),
     slug:varchar('slug', { length: 255 }).notNull().unique(),

57e9f2db0c267e4a277cbd0b1fb166b4edaaeb73
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 06:38:29 +0000

    Aug 6, 2026, 12:08 PM

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -2,4 +2,4 @@
 import {auth} from "@lib/auth"
 import {toNextJsHandler} from "better-auth/next-js"
 
-export const authHandler = toNextJsHandler(auth.handler);
+export const {GET,POST} = toNextJsHandler(auth.handler);

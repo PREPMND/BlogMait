8bf137381f1bcbdf52c1ec89ea38fee72779156f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 06:50:05 +0000

    Aug 6, 2026, 12:20 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -5,5 +5,5 @@ export const users = pgTable('users',{
     id:varchar('id',{length:255}).primaryKey(),
     name:varchar('name',{length:255}).notNull(),
     email:varchar('email',{length:255}).notNull().unique(),
-    emailVerified:boolean("email")
+    emailVerified:boolean("email_verfiied").default()
 })

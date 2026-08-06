56245d3694d996b9dbb9a52eab3230b9f91b2244
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 06:55:35 +0000

    Aug 6, 2026, 12:25 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -12,7 +12,8 @@ export const users = pgTable('users', {
 export const sessions = pgTable('sessions', {
     id: varchar('id', { length: 255 }).primaryKey(),
     userId: varchar('user_id', { length: 255 }).references(() => users.id).notNull(),
-    token:varchar('token',)
+    token:varchar('token',{length:255}),
+    
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })

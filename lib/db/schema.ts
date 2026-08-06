cd6ded85b627b162ec730f44048cc93939ca489f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 06:54:43 +0000

    Aug 6, 2026, 12:24 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,20 +1,20 @@
-import {pgTable,varchar,boolean, timestamp} from 'drizzle-orm/pg-core'
+import { pgTable, varchar, boolean, timestamp } from 'drizzle-orm/pg-core'
 
-export const users = pgTable('users',{
-    id:varchar('id',{length:255}).primaryKey(),
-    name:varchar('name',{length:255}).notNull(),
-    email:varchar('email',{length:255}).notNull().unique(),
-    emailVerified:boolean("email_verfiied").default(false),
-    createdAt:timestamp("created_at").defaultNow().notNull(),
-    updatedAt:timestamp("updated_at").defaultNow().notNull(),
+export const users = pgTable('users', {
+    id: varchar('id', { length: 255 }).primaryKey(),
+    name: varchar('name', { length: 255 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull().unique(),
+    emailVerified: boolean("email_verfiied").default(false),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })
 
-export const sessions = pgTable('sessions',{
-    id:varchar('id',{length:255}).primaryKey(),
-    userId:varchar('user_id',{length:255}).references(()=>users.id)
-    name:varchar('name',{length:255}).notNull(),
-    email:varchar('email',{length:255}).notNull().unique(),
-    emailVerified:boolean("email_verfiied").default(false),
-    createdAt:timestamp("created_at").defaultNow().notNull(),
-    updatedAt:timestamp("updated_at").defaultNow().notNull(),
+export const sessions = pgTable('sessions', {
+    id: varchar('id', { length: 255 }).primaryKey(),
+    userId: varchar('user_id', { length: 255 }).references(() => users.id).notNull();
+    name: varchar('name', { length: 255 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull().unique(),
+    emailVerified: boolean("email_verfiied").default(false),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })

5c3bb063a89857430043c54306fa21aee558456f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 07:03:15 +0000

    Aug 6, 2026, 12:33 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -12,7 +12,7 @@ export const users = pgTable('users', {
 export const sessions = pgTable('sessions', {
     id: varchar('id', { length: 255 }).primaryKey(),
     userId: varchar('user_id', { length: 255 }).references(() => users.id).notNull(),
-    
+    token:varchar('token',{length:255}),
     expiresAt: timestamp("expires_at").notNull(),
     ipAddess:varchar('ip_address',{length:255}),
     userAgent:text('user_agent'),
@@ -23,7 +23,7 @@ export const sessions = pgTable('sessions', {
 export const accounts = pgTable('accounts', {
     id: varchar('id', { length: 255 }).primaryKey(),
     userId: varchar('user_id', { length: 255 }).references(() => users.id).notNull(),
-    token:varchar('token',{length:255}),
+    account
 
     expiresAt: timestamp("expires_at").notNull(),
     ipAddess:varchar('ip_address',{length:255}),

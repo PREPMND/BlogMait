f1f3222c36e3aeb846db452ad8167d4d202496b4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 05:24:16 +0000

    Aug 6, 2026, 10:54 AM

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -23,7 +23,7 @@ export default function RegisterForm() {
         defaultValues: {
             email: "",
             password: "",
-            
+            confirmPassword:"",
         },
     });
 

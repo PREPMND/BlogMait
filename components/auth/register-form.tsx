eca5b80cac87894c576df3eb1b2834444503978a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 14:02:17 +0000

    Aug 6, 2026, 7:32 PM

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import { signUp } from "@/lib/auth-client";
+import { useRouter } from "next/router";
 
 const RegisterSchema = {
     username: string().min(4).max(20),
@@ -25,7 +26,7 @@ export default function RegisterForm() {
     const [errorMessage, setErorrMesssage] = useState<string | undefined>("");
     const [success, setSuccess] = useState(false);
     const [loading, setLoading] = useState(false); 
-
+    const router=useRouter
     const form = useForm<RegisterFormValues>({
         defaultValues: {
             email: "",

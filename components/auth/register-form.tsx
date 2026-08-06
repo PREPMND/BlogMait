2078e020b17c8846d7b4080bf5096cce47784224
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 05:42:35 +0000

    Aug 6, 2026, 11:12 AM

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -126,7 +126,7 @@ export default function RegisterForm() {
                     </div>
                 )}
             </div>
-            <Button className={`w-[110px] mx-auto py-4 border-bsy-1 border-l-sky-100`} type="submit">
+            <Button className={`w-[110px] mx-auto py-4 border-b-sky100 border-l-sky-100`} type="submit">
                 Register
             </Button>
         </form>

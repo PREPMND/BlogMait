6db9b5dc45d26552f832a8abb85e64790cfc099c
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 06:48:14 +0000

    Aug 6, 2026, 12:18 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,9 +1,8 @@
-import { isNotNull } from 'drizzle-orm'
-import { primaryKey } from 'drizzle-orm/gel-core'
 import { varchar } from 'drizzle-orm/mysql-core'
 import {pgTable} from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users',{
     id:varchar('id',{length:255}).primaryKey(),
     name:varchar('name',{length:255}).notNull(),
+    
 })

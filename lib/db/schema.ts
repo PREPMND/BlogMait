dffd2b917220a8d340c7696645c46d654ee6a1e4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 06:56:04 +0000

    Aug 6, 2026, 12:26 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -14,6 +14,6 @@ export const sessions = pgTable('sessions', {
     userId: varchar('user_id', { length: 255 }).references(() => users.id).notNull(),
     token:varchar('token',{length:255}),
 
-    expiresAt: timestamp("created_at").defaultNow().notNull(),
+    expiresAt: timestamp("expires_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })

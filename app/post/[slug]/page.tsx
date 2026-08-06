7a09bc8d5441d7cdb379447e2264b980de07492e
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 14:29:45 +0000

    Aug 6, 2026, 7:59 PM

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -5,7 +5,7 @@ export default function PostDetail(){
         <>
         <div>
             post detail
-            <BlogCard title="hey" description=""
+            <BlogCard title="hey" description="hey" slug=""
         </div>
         </>
     )

dd1433f33cedb7fb98c4004380b1856975826612
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 14:24:31 +0000

    Aug 6, 2026, 7:54 PM

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${playfair.variable} ${poppins.variable} ${quicksand.variable} h-full antialiased`}
     >
       <body className="h-full flex flex-col">
-        <ThemeProvider attribute={'class'}  defaultTheme="system" enableSystem disableTransitionOnChange containerClassName=" md:w-[70%]  w-[99%]  border-1 min-h-[100vh] dark:bg-neutral-900 bg-white/70 border-gray-300 rounded-lg shadow-sm border-2 border-slate-900 rounded-xl shadow-[4px_4px_0px_0px_rgba(56,189,248,1)]
+        <ThemeProvider attribute={'class'}  defaultTheme="system" enableSystem disableTransitionOnChange containerClassName=" md:w-[70%]  w-[99%]  border-1 min-h-[100vh] dark:bg-neutral-900 bg-white/70 border-gray-300 rounded-lg shadow-sm shadow-[4px_4px_0px_0px_rgba(56,189,248,1)]
   dark:border-t-neutral-950 dark:border-b-neutral-950  dark:border-l-neutral-700 dark:border-r-neutral-700">
           {children}
         </ThemeProvider>

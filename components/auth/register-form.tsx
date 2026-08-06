a6a31ad4b27696c3704c9841c15c76d6dc92b027
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 05:45:48 +0000

    Aug 6, 2026, 11:15 AM

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -126,7 +126,7 @@ export default function RegisterForm() {
                     </div>
                 )}
             </div>
-            <Button className={`w-[110px] mx-auto py-4 border-b-4 border-r-3 hover:scale-[1.03] transition-all duration-500 ease-in-out hover:border-b-sky-100 hover:border-r-`} type="submit">
+            <Button className={`w-[110px] mx-auto py-4 border-b-4 border-r-3 hover:scale-[1.03] transition-all duration-500 ease-in-out hover:border-b-sky-100 hover:border-r-indigo-200`} type="submit">
                 Register
             </Button>
         </form>

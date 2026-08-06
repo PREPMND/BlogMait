fb813df81d84f9a2bd80708d645e5219cd2baaf4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 11:32:01 +0000

    Aug 6, 2026, 5:02 PM

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
 import { betterAuth } from "better-auth";
-import {} from "better-auth/adapters/drizzle"
+import {drizzleAdapter} from "better-auth/adapters/drizzle"
 export const auth=betterAuth({
     appName:"Blog Next",
     secret:process.env.BETTER_AUTH_SECRET || '',

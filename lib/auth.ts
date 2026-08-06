cf2ed9599f0df03bb201fea5e12bfad7d38f58f9
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 06:21:30 +0000

    Aug 6, 2026, 11:51 AM

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 
 export const auth=betterAuth({
     appName:"Blog Next",
-    secret:process.env.AUTH_SECRET_KEY || '',
+    secret:process.env.BETTER_AUTH_SECRET || '',
     emailAndPassword:{
         enabled:true,
     }

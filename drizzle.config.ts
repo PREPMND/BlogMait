ec484f3768ce68e8194c086650a1915f246266b6
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 11:51:13 +0000

    Aug 6, 2026, 5:21 PM

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default ({
     schema:'./lib/db/schema.ts',
     out:"./drizzle",
     dbCredentials:{
-        url:process.env.DATABASE_URL || '',
+        url:process.env.DATABASE_URL,
     },
     verbose:true,
     strict:true

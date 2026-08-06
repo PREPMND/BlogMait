2c0a7c24d215eca902568cbeb9a7012327584d68
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 07:04:02 +0000

    Aug 6, 2026, 12:34 PM

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -25,10 +25,10 @@ export const accounts = pgTable('accounts', {
     userId: varchar('user_id', { length: 255 }).references(() => users.id).notNull(),
     accountId :varchar('account_id', { length: 255 }).notNull(),
     providerId:varchar('provider_id', { length: 255 }).notNull(),
-    
+
     expiresAt: timestamp("expires_at").notNull(),
     ipAddess:varchar('ip_address',{length:255}),
-    userAgent:text('user_agent'),
+    password:text('password'),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })

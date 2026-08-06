8f280d7b4582a77f70b5f4b54de5331204fb1d02
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 05:25:25 +0000

    Aug 6, 2026, 10:55 AM

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -33,7 +33,7 @@ export default function RegisterForm() {
     const onSubmit = (data: RegisterFormValues) => {
         const email = RegisterSchema.email.safeCheck(data.email);
         const password = RegisterSchema.password.safeCheck(data.password);
-        const 
+        const confirmPassword =Regi
         if (!email.ok) {
             console.log(email.error);
             seterrorRegister(true);

a815518c3c91724f66694b49579230e92d1684b4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-06 11:29:04 +0000

    Aug 6, 2026, 4:59 PM

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -5,5 +5,7 @@ import * as schema from "./schema"
 const pool = new Pool({
     connectionString:process.env.DATABASE_URL,
     ssl:
-    process.env.NODE_ENV==='production'?
+    process.env.NODE_ENV==='production'?{
+        rejectUnauthorized:
+    }
 })

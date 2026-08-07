a73c6c368881c0e735571d0207dd9fec6ec00f78
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-07 03:45:00 +0000

    Aug 7, 2026, 9:15 AM

--- components/ui/card-tactical.tsx
+++ components/ui/card-tactical.tsx
@@ -106,7 +106,7 @@ export default function Perfect3DTiltCard({ id, title, description, slug,image }
                     <h3 className="text-lg line-clamp-1 font-bold tracking-tight text-neutral-800 pb-1 leading-snug">
                         {title}
                     </h3>
-                    <img className="object-cover rounded-md" src={image}/>
+                    <img className="object-cover rounded-md" src={image} />
 
                     <p className="text-sm text-neutral-400 line-clamp-2 leading-relaxed">
                         {description}

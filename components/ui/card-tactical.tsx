7071ac5bd98abc9450f3879187780918774c888b
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-07 03:39:56 +0000

    Aug 7, 2026, 9:09 AM

--- components/ui/card-tactical.tsx
+++ components/ui/card-tactical.tsx
@@ -102,11 +102,8 @@ export default function Perfect3DTiltCard({ id, title, description, slug }: Blog
                     className="pt-1 pointer-events-none"
                     style={{ transform: "translateZ(20px)" }}
                 >
-                    <span className="text-xs font-semibold text-neutral-700 tracking-wider">
-                        ARTICLE #{id}
-                    </span>
 
-                    <h3 className="text-xl line-clamp-1 font-bold tracking-tight text-neutral-100 leading-snug">
+                    <h3 className="text-lg line-clamp-1 font-bold tracking-tight text-neutral-100 leading-snug">
                         {title}
                     </h3>
                     <img className="object-cover rounded-md" src='https://images.unsplash.com/photo-1785970869989-91d5fcc43712?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8'/>

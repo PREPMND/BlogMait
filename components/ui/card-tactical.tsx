6385419d933eb77efb5d51249aa7db56a10c493b
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-07 03:11:33 +0000

    Aug 7, 2026, 8:41 AM

--- components/ui/card-tactical.tsx
+++ components/ui/card-tactical.tsx
@@ -32,8 +32,8 @@ export default function Perfect3DTiltCard({ id, title, description, slug }: Blog
         const mouseX = e.clientX - rect.left;
         const mouseY = e.clientY - rect.top;
 
-        const rX = (mouseY / rect.height - 0.5) * -24;
-        const rY = (mouseX / rect.width - 0.5) * 24;
+        const rX = (mouseY / rect.height - 0.5) * -16;
+        const rY = (mouseX / rect.width - 0.5) * 16;
 
         rotateX.set(rX);
         rotateY.set(rY);

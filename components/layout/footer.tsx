4046ca1ef80bad98464e9b9e193a2e34db441803
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-07 04:04:09 +0000

    Aug 7, 2026, 9:34 AM

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -101,7 +101,7 @@ export function Footer() {
                                     href="#"
                                     className="group flex items-center justify-center gap-3 text-neutral-600 transition hover:text-sky-500 dark:text-neutral-400 dark:hover:text-white lg:justify-end"
                                 >
-                                    <Linkedin className="h-5 w-5" />
+                                    <Linked className="h-5 w-5" />
                                     LinkedIn
                                     <ArrowUpRight className="h-4 w-4 transition-transform duration-200 group-hover:-translate-y-1 group-hover:translate-x-1" />
                                 </Link>

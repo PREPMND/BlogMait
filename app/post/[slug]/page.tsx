2bae78e0ccdd5822c8e6851a622a7fb325a984af
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-07 03:41:57 +0000

    Aug 7, 2026, 9:11 AM

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -16,7 +16,7 @@ export default function PostDetail(){
             title:'hey',description:'hey',slug:'hey',id:3,image:"https://images.unsplash.com/photo-1781882915155-2a5355116a74?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2MHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
-            title:'hey',description:'hey',slug:'hey',id:4
+            title:'hey',description:'hey',slug:'hey',id:4,image:"https://plus.unsplash.com/premium_photo-1785584592505-1f54b5c1e854?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3NXx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             title:'hey',description:'hey',slug:'hey',id:5

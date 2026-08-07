a45f316c3c5b618e5839647dd0f37ec90af7791a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-07 03:34:29 +0000

    Aug 7, 2026, 9:04 AM

--- components/ui/card-tactical.tsx
+++ components/ui/card-tactical.tsx
@@ -111,7 +111,7 @@ export default function Perfect3DTiltCard({ id, title, description, slug }: Blog
                     </h3>
                     <img src='https://images.unsplash.com/photo-1785970869989-91d5fcc43712?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8'/>
 
-                    <p className="text-sm text-neutral-400 line-clamp-3 leading-relaxed">
+                    <p className="text-sm text-neutral-400 line-clamp-2 leading-relaxed">
                         {description}
                     </p>
                 </div>

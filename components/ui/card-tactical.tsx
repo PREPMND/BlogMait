0d4390ba01a305ce529e152b967cd265a6d7d62b
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-08-07 03:13:25 +0000

    Aug 7, 2026, 8:43 AM

--- components/ui/card-tactical.tsx
+++ components/ui/card-tactical.tsx
@@ -80,7 +80,7 @@ export default function Perfect3DTiltCard({ id, title, description, slug }: Blog
                     rotateY: rotateYSpring,
                     transformStyle: "preserve-3d",
                 }}
-                className="glowing-card max-w-[99%] m-auto w-[360px] h-[380px] xs:w-[210px] xss:h-[280px] xss:w-[230px] xs:h-[240px]  sm:w-[250px] md:w-[300px] lg:w-[250px] md:h-[300px] p-6 bg-neutral-100  dark:bg-neutral-900 text-neutral-100 cursor-pointer border border-neutral-300 dark:border-neutral-700 rounded-2xl flex flex-col justify-between transition-colors duration-200"
+                className="glowing-card max-w-[99%] m-auto w-[360px] h-[380px] xs:w-[210px] xss:h-[280px] xss:w-[230px] xs:h-[240px]  sm:w-[250px] md:w-[300px] lg:w-[250px] md:h-[300px] p-6 bg-neutral-100 bg-gradient-to-br from-neutral-900 via-neutral-900 to-neutral-800  dark:bg-neutral-900 text-neutral-100 cursor-pointer border border-neutral-300 dark:border-neutral-700 rounded-2xl flex flex-col justify-between transition-colors duration-200"
             >
                 <div
                     className="space-y-3 pointer-events-none"
